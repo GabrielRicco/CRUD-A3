3047c6e0703c483f6047a98e16e5c5c0c8894d70
Gabriel Ricco <gabrielrdantas@outlook.com>
2024-06-06 09:55:09 -0300

    Adicionando método de deletar ligação entre estudante e curso e melhorando script para verificação de estudante ja existente no curso

--- Controllers/SchoolController.cs
+++ Controllers/SchoolController.cs
@@ -35,9 +35,35 @@ namespace CrudMongoApp.Controllers
 
                     return Ok("Estudante " + student.Name + " adicionado ao curso de " + course.Name);
                 }
+
+                return BadRequest("Estudante já está matriculado no curso");
             }
 
-            return BadRequest();
+            return BadRequest("Estudante ou curso não encontrado");
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteStudentToCourse(Guid studentId, Guid courseId)
+        {
+            var student = await _studentService.GetByIdAsync(studentId);
+            var course = await _courseService.GetByIdAsync(courseId);
+
+            if (student != null && course != null)
+            {
+                student.CourseId = null;
+                if (course.Students!.Contains(student))
+                {
+                    course.Students.Remove(student);
+                    await _studentService.UpdateAsync(studentId, student);
+                    await _courseService.UpdateAsync(courseId, course);
+
+                    return Ok("Estudante " + student.Name + " removido do curso de " + course.Name);
+                }
+
+                return BadRequest("Estudante não está matriculado no curso");
+            }
+
+            return BadRequest("Estudante ou curso não encontrado");
         }
     }
 }
--- Models/Student.cs
+++ Models/Student.cs
@@ -1,8 +1,32 @@
 namespace Models;
 
-public class Student
+public class Student : IEquatable<Student>
 {
     public Guid Id { get; set; }
     public string? Name { get; set; }
     public Guid? CourseId { get; set; }
+
+    public bool Equals(Student? other)
+    {
+        if (other == null)
+            return false;
+
+        return this.Id == other.Id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+            return false;
+
+        if (obj is not Student studentObj)
+            return false;
+
+        return Equals(studentObj);
+    }
+
+    public override int GetHashCode()
+    {
+        return this.Id.GetHashCode();
+    }
 }
